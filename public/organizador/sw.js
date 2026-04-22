9dd8e60828af2d4fc043e8015122cb03a8e77b12
Matias Rojo <maturojo123mdq@gmail.com>
2026-04-22 10:03:55 -0300

    Agregar archivos Organizador PWA

---
+++ public/organizador/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "organizador-v1.2.0";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./manifest.json"
+];
+
+// Instalar: cachear assets base
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activar: borrar caches viejos
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: network-first con fallback a cache
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
